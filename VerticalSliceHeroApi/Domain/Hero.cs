21e1a31d02ce62d99359de075d7d6659f2c5f6b6
Vincent Nacar <vincent.nacar@contractor.fitlg.com>
2023-09-21 22:29:13 +0800

    fix warnings

--- VerticalSliceHeroApi/Domain/Hero.cs
+++ VerticalSliceHeroApi/Domain/Hero.cs
@@ -2,8 +2,8 @@
 {
     public class Hero
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public int Level { get; set; }
     }
 }
