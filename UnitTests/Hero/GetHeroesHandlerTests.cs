8fd901d6b4e5c4da3f36e235d8396d8361b14235
Vincent Nacar <vincent.nacar@contractor.fitlg.com>
2023-09-21 22:20:34 +0800

    added tests

---
+++ UnitTests/Hero/GetHeroesHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VerticalSliceHeroApi.Features.Heroes.Query;
+using VerticalSliceHeroApi.Infrastructure;
+
+namespace UnitTests.Hero
+{
+    [TestClass]
+    public class GetHeroesHandlerTests
+    {
+        private IRequestHandler<GetHeroesQuery, GetHeroesResponse> _handler;
+
+        [TestInitialize] 
+        public void Initialize() {
+            var db = new FakeDb();
+            _handler = new GetHeroesHandler(db);
+        }
+
+        [TestMethod]
+        public async Task GetHeroes()
+        {
+            var heroes = await _handler.Handle(new GetHeroesQuery(), cancellationToken: default);
+            Assert.IsNotNull(heroes);
+        }
+
+        [TestMethod]
+        public async Task GetHeroes_ReturnSingle()
+        {
+            var query = new GetHeroesQuery();
+            query.Name = "Spiderman";
+            var heroes = await _handler.Handle(query, cancellationToken: default);
+            Assert.IsNotNull(heroes);
+            Assert.AreEqual(1, heroes.Total);
+        }
+    }
+}
